8c2b2128f5a8b3d8e90753c9086aad1c2aeae307
Ruwaid Al-Habsi <2230072a@student.gla.ac.uk>
2017-02-09 12:08:09 +0000

    part 7 complete

--- rango/views.py
+++ rango/views.py
@@ -4,6 +4,7 @@ from django.http import HttpResponse
 from rango.forms import CategoryForm
 from rango.models import Category
 from rango.models import Page
+from rango.forms import PageForm
 
 def show_category(request, category_name_slug):
     
@@ -31,6 +32,28 @@ def add_category(request):
         else:
             print(form.errors)
     return render(request, 'rango/add_category.html', {'form': form})
+
+def add_page(request, category_name_slug):
+    try:
+        category = Category.objects.get(slug=category_name_slug)
+    except Category.DoesNotExist:
+        category = None
+
+    form = PageForm()
+    if request.method == 'POST':
+        form = PageForm(request.POST)
+        if form.is_valid():
+            if category:
+                page = form.save(commit=False)
+                page.category = category
+                page.views = 0
+                page.save()
+                return show_category(request, category_name_slug)
+        else:
+            print(form.errors)
+            
+    context_dict = {'form': form, 'category': category}
+    return render(request, 'rango/add_page.html', context_dict)
 
 def index(request):
    # Query the database for a list of ALL categories currently stored.
